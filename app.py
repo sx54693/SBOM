6b78bd76ee6efbfcf3bab9bca418dc311453e55c
sx54693 <103083868+sx54693@users.noreply.github.com>
2025-03-20 12:47:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -214,6 +214,8 @@ def display_sbom_data(sbom_data, file_path):
 if generate_button and file1:
     file1_path = save_uploaded_file(file1)
     sbom_output = generate_sbom(file1_path)
+    st.write("🔍 SBOM Output File Path:", sbom_output)
+
     
     if sbom_output:
         with open(sbom_output, "r", encoding="utf-8") as f:
