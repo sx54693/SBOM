d11b3cf7a17249e4156b00108c05341674313a02
sx54693 <103083868+sx54693@users.noreply.github.com>
2025-03-25 17:01:39 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Create a writable bin directory
-mkdir -p ./bin
+# Ensure pip installs uvicorn
+pip install -r requirements.txt
 
-# Install Syft into ./bin (inside your project)
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ./bin
-
-# Make sure ./bin is in the PATH
-echo 'export PATH=$PATH:./bin' >> ~/.profile
