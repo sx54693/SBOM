80604f6c5a74dc07b516050a27969fb043759c68
sx54693 <103083868+sx54693@users.noreply.github.com>
2025-03-25 00:03:43 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+
+set -e  # Exit on error
+
+# 🛠️ Install Syft CLI to /usr/local/bin
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+# 🐍 Install Python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
 
