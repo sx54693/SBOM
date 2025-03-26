694781ca2e781512bfe62a1e9052de64523ec88c
sx54693 <103083868+sx54693@users.noreply.github.com>
2025-03-25 22:00:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from sbom_generator import generate_sbom
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "✅ SBOM API is working"}
+    return {"message": "✅ SBOM API is working at /generate-sbom/"}
+
+# Register your SBOM endpoint
+app.post("/generate-sbom/")(generate_sbom)
